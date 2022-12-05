4a90f71c3963a2cae4205bafc637a7b91dead71e
lgaid <104643045+lgaid@users.noreply.github.com>
2022-12-05 13:00:40 -0500

    Spring Rest

---
+++ src/test/java/com/cydeo/ticketingprojectrestlng/TicketingProjectRestLngApplicationTests.java
@@ -0,0 +1,13 @@
+package com.cydeo.ticketingprojectrestlng;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TicketingProjectRestLngApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
